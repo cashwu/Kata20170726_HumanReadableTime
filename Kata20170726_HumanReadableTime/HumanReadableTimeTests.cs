7fb3642aef281a4f4ef314772f5de161f72e6e17
cash <cash@cashwu.com>
2017-07-26 16:47:54 +0800

    refactor

--- Kata20170726_HumanReadableTime/HumanReadableTimeTests.cs
+++ Kata20170726_HumanReadableTime/HumanReadableTimeTests.cs
@@ -10,9 +10,14 @@ namespace Kata20170726_HumanReadableTime
         [TestMethod]
         public void input_0_readableTime_shouldbe_000000()
         {
+            AssertGetReadableTimeShouldBe(0, "00:00:00");
+        }
+
+        private void AssertGetReadableTimeShouldBe(int seconds, string expected)
+        {
             var timeFormat = new TimeFormat();
-            var actual = timeFormat.GetReadableTime(0);
-            Assert.AreEqual("00:00:00", actual);
+            var actual = timeFormat.GetReadableTime(seconds);
+            Assert.AreEqual(expected, actual);
         }
     }
 
