82f54ea9e07f3ac5c197a68e2b01f5713fff14a5
cash <cash@cashwu.com>
2017-07-26 16:46:55 +0800

    input 0 test case

--- Kata20170726_HumanReadableTime/HumanReadableTimeTests.cs
+++ Kata20170726_HumanReadableTime/HumanReadableTimeTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170726_HumanReadableTime
@@ -7,8 +8,19 @@ namespace Kata20170726_HumanReadableTime
     public class HumanReadableTimeTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_0_readableTime_shouldbe_000000()
         {
+            var timeFormat = new TimeFormat();
+            var actual = timeFormat.GetReadableTime(0);
+            Assert.AreEqual("00:00:00", actual);
+        }
+    }
+
+    public class TimeFormat
+    {
+        public string GetReadableTime(int seconds)
+        {
+            return "00:00:00";
         }
     }
 }
